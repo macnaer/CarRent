3f1d3e0f9de2d2d61b19e8a9565398e01efea1df
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-05-27 10:54:10 +0300

    Start with templates

---
+++ header.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Car Rent &mdash; Free Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/aos.css">
+
+    <!-- MAIN CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+
+    <div class="site-wrap" id="home-section">
+
+        <div class="site-mobile-menu site-navbar-target">
+            <div class="site-mobile-menu-header">
+                <div class="site-mobile-menu-close mt-3">
+                    <span class="icon-close2 js-menu-toggle"></span>
+                </div>
+            </div>
+            <div class="site-mobile-menu-body"></div>
+        </div>
+
+
+
+        <header class="site-navbar site-navbar-target" role="banner">
+
+            <div class="container">
+                <div class="row align-items-center position-relative">
+
+                    <div class="col-3 ">
+                        <div class="site-logo">
+                            <a href="index.html">CarRent</a>
+                        </div>
+                    </div>
+
+                    <div class="col-9  text-right">
+
+
+                        <span class="d-inline-block d-lg-none"><a href="#" class="text-white site-menu-toggle js-menu-toggle py-5 text-white"><span class="icon-menu h3 text-white"></span></a></span>
+
+
+
+                        <nav class="site-navigation text-right ml-auto d-none d-lg-block" role="navigation">
+                            <ul class="site-menu main-menu js-clone-nav ml-auto ">
+                                <li class="active"><a href="index.html" class="nav-link">Home</a></li>
+                                <li><a href="services.html" class="nav-link">Services</a></li>
+                                <li><a href="cars.html" class="nav-link">Cars</a></li>
+                                <li><a href="about.html" class="nav-link">About</a></li>
+                                <li><a href="blog.html" class="nav-link">Blog</a></li>
+                                <li><a href="contact.html" class="nav-link">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+
+
+                </div>
+            </div>
+
+        </header>
+
+        <div class="ftco-blocks-cover-1">
+            <div class="ftco-cover-1 overlay" style="background-image: url('images/hero_1.jpg')">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-5">
+                            <div class="feature-car-rent-box-1">
+                                <h3>Range Rover S7</h3>
+                                <ul class="list-unstyled">
+                                    <li>
+                                        <span>Doors</span>
+                                        <span class="spec">4</span>
+                                    </li>
+                                    <li>
+                                        <span>Seats</span>
+                                        <span class="spec">6</span>
+                                    </li>
+                                    <li>
+                                        <span>Lugage</span>
+                                        <span class="spec">2 Suitcase/2 Bags</span>
+                                    </li>
+                                    <li>
+                                        <span>Transmission</span>
+                                        <span class="spec">Automatic</span>
+                                    </li>
+                                    <li>
+                                        <span>Minium age</span>
+                                        <span class="spec">Automatic</span>
+                                    </li>
+                                </ul>
+                                <div class="d-flex align-items-center bg-light p-3">
+                                    <span>$150/day</span>
+                                    <a href="contact.html" class="ml-auto btn btn-primary">Rent Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="site-section pt-0 pb-0 bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+
+                        <form class="trip-form">
+                            <div class="row align-items-center mb-4">
+                                <div class="col-md-6">
+                                    <h3 class="m-0">Begin your trip here</h3>
+                                </div>
+                                <div class="col-md-6 text-md-right">
+                                    <span class="text-primary">32</span> <span>cars available</span></span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="form-group col-md-3">
+                                    <label for="cf-1">Where you from</label>
+                                    <input type="text" id="cf-1" placeholder="Your pickup address" class="form-control">
+                                </div>
+                                <div class="form-group col-md-3">
+                                    <label for="cf-2">Where you go</label>
+                                    <input type="text" id="cf-2" placeholder="Your drop-off address" class="form-control">
+                                </div>
+                                <div class="form-group col-md-3">
+                                    <label for="cf-3">Journey date</label>
+                                    <input type="text" id="cf-3" placeholder="Your pickup address" class="form-control datepicker px-3">
+                                </div>
+                                <div class="form-group col-md-3">
+                                    <label for="cf-4">Return date</label>
+                                    <input type="text" id="cf-4" placeholder="Your pickup address" class="form-control datepicker px-3">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <input type="submit" value="Submit" class="btn btn-primary">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
